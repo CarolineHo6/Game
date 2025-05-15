788ae37480d70d990b756d1054fdf48f6deccebe
Caroline Ho <cho@bayviewglen.ca>
2025-05-15 10:11:05 -0400

    Boss and enemies class, Daisy made it but her github isnt working

---
+++ src/Boss.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ 
+public class Boss extends Enemies {
+ 
+    private int stages;
+ 
+    public Boss(int h, int d, String n, int dr, int s) {
+        super(h, d, n, dr, n, n, n, n);
+        stages = s;
+ 
+    }
+ 
+    public void nextStage(int addDamage, int addHealth) {
+        super.setHealth(addHealth);
+        super.setDamage(addDamage);
+        stages--;
+ 
+    }
+ 
+    public int getStages() {
+        return stages;
+ 
+    }
+ 
+}
+ 
+ 
---
+++ src/Enemies.java
@@ -0,0 +1,48 @@
+import java.util.*;
+ 
+public class Enemies extends NPC {
+    private int health;
+    private int damage;
+    private String name;
+    private int dodgeRange;
+ 
+    // caroline fix this, I forgot to add the super stuff.
+    public Enemies(int h, int d, String n, int dr, String name, String currentRoom, String description, String talk) {
+        super(name, currentRoom, true, description, talk);
+        health = h;
+        damage = d;
+        name = n;
+        dodgeRange = dr;
+    }
+ 
+    public int getHealth() {
+        return health;
+    }
+ 
+    public void setHealth(int m) {
+        health = m;
+    }
+ 
+    public int getDamage() {
+        return damage;
+    }
+ 
+    public void setDamage(int x) {
+        damage = x;
+    }
+ 
+    public String getName(int m) {
+        return name;
+    }
+ 
+    public boolean ifDodge() {
+ 
+        int x = (int) (Math.random() * dodgeRange);
+        int y = (int) (Math.random() * dodgeRange);
+ 
+        return x == y;
+ 
+    }
+ 
+}
+ 
