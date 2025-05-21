ed55caf19c1e40a1d6d2f94e2fed8607783e43a7
Catherine Pan <cpan@bayviewglen.ca>
2025-05-21 09:50:24 -0400

    added armor class and some idtems

---
+++ src/Armor.java
@@ -0,0 +1,10 @@
+public class Armor {
+    private int proc;
+    public Armor(String id, String name, String description, int proc){
+        super(id, name, description)
+        this.proc = proc;
+
+    }
+
+    
+}
