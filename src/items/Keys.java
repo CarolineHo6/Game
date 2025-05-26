802d18877d7ad28cd42c3cd978438f2602689fa0
Cindy Yan <xyan@bayviewglen.ca>
2025-05-26 12:15:18 -0400

    keys class

--- src/items/Keys.java
+++ src/items/Keys.java
@@ -1,5 +1,15 @@
-import java.util.*;
+package items;
 
 public class Keys extends Item {
+    private String room;
+    
+    public Keys (String id, String name, String description, String type, String room){
+        super(id, name, description, type);
+        this.room = room;
+    }
+
+    public String getRoom(){
+        return room;
+    }
 
 }
